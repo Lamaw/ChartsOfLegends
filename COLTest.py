c02936272c1ab556e5bafac941812151b7a4b4a1
Antoine MAHOUIN <antoinex.mahouin@intel.com>
2015-04-21 14:08:12 +0200

    test file added

---
+++ COLTest.py
@@ -0,0 +1,17 @@
+from WebClient import WebClient
+import xmlParser
+import PageAnalyzer
+
+#PageAnalyzer Instanciation
+
+config = xmlParser.ParsedConfig()
+client = WebClient("http://leagueoflegends.wikia.com/wiki","http://proxy-jf.intel.com:911")
+analyzer = PageAnalyzer.PageAnalyzer(config.config)
+
+response = client.get_web_page("champions")
+
+page = response.read()
+
+champ_list = analyzer.extract_champ_list(page)
+for champ in champ_list:
+    print champ
